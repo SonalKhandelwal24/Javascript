f2e6297a936f75df5e1cf98bd7e561b85deaa2cd
Sonal Khandelwal <128227335+JavaCodeWithSonal@users.noreply.github.com>
2024-06-19 13:58:58 +0000

    container config done

---
+++ .devcontainer/basics/ColorChanger.js
@@ -0,0 +1,26 @@
+//generate the random color---
+const randomColor = function(){
+    let hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+console.log(randomColor());
+// console.log(Math.floor(Math.random() * 16));
+//16 is the position of the random number
+
+let intervalId;
+const startBgColor = function () {
+    intervalId = setInterval(function(){
+        document.body.style.backgroundColor = randomColor();
+    }, 1000);
+}
+document.querySelector('#start').addEventListener('click', startBgColor)
+
+const stopBgColor = function () {
+    clearInterval(intervalId);
+    intervalId(null);
+}
+document.querySelector('#stop').addEventListener('click', stopBgColor);
