6c422243d4ad8d68ec2782f5e046e3347336bd2c
Sonal Khandelwal <128227335+JavaCodeWithSonal@users.noreply.github.com>
2024-06-20 05:31:28 +0000

    container config done

--- .devcontainer/basics/ColorChanger.js
+++ .devcontainer/basics/ColorChanger.js
@@ -1,34 +1,28 @@
 //generate the random color---
-const randomColor = function(){
-    let hex = '0123456789ABCDEF';
-    let color = '#';
+const randomColor = function () {
+    let hex = "0123456789ABCDEF";
+    let color = "#";
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
 };
 console.log(randomColor());
-// console.log(Math.floor(Math.random() * 16));
-//16 is the position of the random number
 
-let intervalId;
 //method 1 ---
-// const startBgColor = function () {
-//     intervalId = setInterval(function(){
-//         document.body.style.backgroundColor = randomColor();
-//     }, 1000);
-// }
-// document.querySelector('#start').addEventListener('click', startBgColor);
-
-//method 2 ---
-document.querySelector('#start').addEventListener('click', function () {
-    intervalId = setInterval(function(){
+let intervalId;
+function startBgColor() {
+    intervalId = setInterval(function () {
         document.body.style.backgroundColor = randomColor();
     }, 1000);
-})
+}
+const start = document.querySelector("#start");
+start.addEventListener("click", startBgColor);
+// Callback Function: Pass the reference to the function startBgColor without invoking it (i.e., without ())---
 
 const stopBgColor = function () {
     clearInterval(intervalId);
     intervalId(null);
-}
-document.querySelector('#stop').addEventListener('click', stopBgColor);
+};
+const stop = document.querySelector("#stop");
+stop.addEventListener("click", stopBgColor);
